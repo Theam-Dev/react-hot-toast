8b3bb4f5061c32e376f378f62b4541d031256bf4
Theam-Dev <phearakroun@gmail.com>
2023-05-14 16:49:02 +0700

    Hot Toast

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
+  const mystyle = {
+    color: "white",
+    backgroundColor: "#960899",
+    padding: "10px",
+    borderRadius: "5px",
+    border: "1px solid #ff000000",
+    margin: "5px",
+    cursor: "pointer"
+  };
+
+  const topright = () => toast.success('Hello World', {
+    duration: 1500,
+    position: 'top-right',
+    style: {
+      background:"green",
+      color:"white"
+    },
+  });
+  const bottomright =()=>toast.error('Hello World', {
+    duration: 1500,
+    position: 'bottom-right',
+    style: {
+      background:"red",
+      color:"white"
+    },
+  });
+  const topleft =()=>toast.loading('Hello World', {
+    duration: 1500,
+    position: 'top-left',
+    style: {
+      background:"purple",
+      color:"white"
+    },
+  });
+  const bottomleft =()=>toast.success('Hello World', {
+    duration: 1500,
+    position: 'bottom-left',
+    style: {
+      background:"green",
+      color:"white"
+    },
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>React using react-hot-toast</h2>
+      <button onClick={topright} style={mystyle}>Top Right</button>
+      <button onClick={bottomright} style={mystyle}>Bottom Right</button>
+      <button onClick={topleft} style={mystyle}>Top Left</button>
+      <button onClick={bottomleft} style={mystyle}>Bottom Left</button>
+      <Toaster />
     </div>
   );
 }
